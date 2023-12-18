d86bbfd03b18762d6104c506c4e555d42ec42f0d
natol2023 <moneta.ya@gmail.com>
2023-12-18 12:46:12 +0100

    git@github.com:natol2023/lesson1.git

--- src/main/java/de/telran/Main.java
+++ src/main/java/de/telran/Main.java
@@ -2,6 +2,11 @@ package de.telran;
 
 public class Main {
     public static void main(String[] args) {
+        public static void test (){
+            return 10:
+
+        }
         System.out.println("Hello world!");
+
     }
 }
